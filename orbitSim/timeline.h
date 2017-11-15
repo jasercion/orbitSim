9e63618266a75ec47b70e581c5cf82b3fa8e8182
Alex Reustle <areustledev@gmail.com>
2017-11-15 17:45:51 -0500

    rerwite initial and header rules to better support old compilers. Remove std=c++0x flag in Makefile

--- orbitSim/timeline.h
+++ orbitSim/timeline.h
@@ -88,7 +88,7 @@ struct timeline_event
   opt_evt_fields additional;
 };
 
-struct initial{
+struct timeline_initial{
   int week;
   std::string timeline_name;
   std::string create_time;
@@ -97,30 +97,30 @@ struct initial{
   double DEC;
   rocking_profile profile;
   timeline_event event;
-  std::vector<std::string> tako_db;
-  std::vector<std::string> sc_ephem;
-  std::vector<std::string> saa;
-  std::vector<std::string> tdrss_ephem;
-  std::vector<std::string> tdrss_sched;
-  std::string prev_arr_thresh;
+  /* std::vector<std::string> tako_db; */
+  /* std::vector<std::string> sc_ephem; */
+  /* std::vector<std::string> saa; */
+  /* std::vector<std::string> tdrss_ephem; */
+  /* std::vector<std::string> tdrss_sched; */
+  /* std::string prev_arr_thresh; */
 };
 
-struct timeline_header
-{
-  std::string filename;
-  std::string creation_time;
-  std::string mission_id;
-  std::string originator;
-  std::string db_version;
-  std::string dest_processor;
-  std::string start_time;
-  std::string stop_time;
-  std::string execute_flag;
-  std::string timeline_type;
-  std::string version_num;
-  std::string ref_timeline_name;
-  std::string comment;
-};
+/* struct timeline_header */
+/* { */
+/*   std::string filename; */
+/*   std::string creation_time; */
+/*   std::string mission_id; */
+/*   std::string originator; */
+/*   std::string db_version; */
+/*   std::string dest_processor; */
+/*   std::string start_time; */
+/*   std::string stop_time; */
+/*   std::string execute_flag; */
+/*   std::string timeline_type; */
+/*   std::string version_num; */
+/*   std::string ref_timeline_name; */
+/*   std::string comment; */
+/* }; */
 
 struct command
 {
@@ -133,8 +133,9 @@ struct command
 
 struct timeline_wrapper
 {
-  timeline_header header;
-  initial init;
+  /* timeline_header header; */
+  std::string header;
+  timeline_initial init;
   //std::vector<command> commands;
   std::vector<timeline_event> events;
 };
@@ -173,7 +174,7 @@ BOOST_FUSION_ADAPT_STRUCT(
     )
 
 BOOST_FUSION_ADAPT_STRUCT(
-    initial,
+    timeline_initial,
     (int, week)
     (std::string, timeline_name)
     (std::string, create_time)
@@ -182,14 +183,13 @@ BOOST_FUSION_ADAPT_STRUCT(
     (double, DEC)
     (rocking_profile, profile)
     (timeline_event, event)
-    (std::vector<std::string>, tako_db)
-    (std::vector<std::string>, sc_ephem)
-    (std::vector<std::string>, saa)
-    (std::vector<std::string>, tdrss_ephem)
-    (std::vector<std::string>, tdrss_sched)
-    (std::string, prev_arr_thresh)
+    /* (std::vector<std::string>, tako_db) */
+    /* (std::vector<std::string>, sc_ephem) */
+    /* (std::vector<std::string>, saa) */
+    /* /1* (std::vector<std::string>, tdrss_ephem) *1/ */
+    /* /1* (std::vector<std::string>, tdrss_sched) *1/ */
+    /* /1* (std::string, prev_arr_thresh) *1/ */
     )
-  /* initial %= */
 
 
 BOOST_FUSION_ADAPT_STRUCT(
@@ -211,27 +211,27 @@ BOOST_FUSION_ADAPT_STRUCT(
     )
 
 
-BOOST_FUSION_ADAPT_STRUCT(
-    timeline_header,
-    (std::string, filename)
-    (std::string, creation_time)
-    (std::string, mission_id)
-    (std::string, originator)
-    (std::string, db_version)
-    (std::string, dest_processor)
-    (std::string, start_time)
-    (std::string, stop_time)
-    (std::string, execute_flag)
-    (std::string, timeline_type)
-    (std::string, version_num)
-    (std::string, ref_timeline_name)
-    (std::string, comment)
-    )
+/* BOOST_FUSION_ADAPT_STRUCT( */
+/*     timeline_header, */
+/*     (std::string, filename) */
+/*     (std::string, creation_time) */
+/*     (std::string, mission_id) */
+/*     (std::string, originator) */
+/*     (std::string, db_version) */
+/*     (std::string, dest_processor) */
+/*     (std::string, start_time) */
+/*     (std::string, stop_time) */
+/*     (std::string, execute_flag) */
+/*     (std::string, timeline_type) */
+/*     (std::string, version_num) */
+/*     (std::string, ref_timeline_name) */
+/*     (std::string, comment) */
+/*     ) */
 
 BOOST_FUSION_ADAPT_STRUCT(
     timeline_wrapper,
-    (timeline_header, header)
-    (initial, init)
+    (std::string, header)
+    (timeline_initial, init)
     //(std::vector<command>, commands)
     (std::vector<timeline_event>, events)
     )
@@ -250,22 +250,14 @@ BOOST_FUSION_ADAPT_STRUCT(
       using qi::repeat;
       using ascii::char_;
       using ascii::digit;
-      using ascii::space;
       using ascii::string;
       using qi::alnum;
-      using qi::blank;
       using boost::spirit::qi::_1;
       using boost::spirit::qi::_val;
       using boost::phoenix::at_c;
 
 
-      file_path %=
-        lexeme[ +( -char_("/") >> +(qi::graph - char_("/") - char_(".")) )
-        >> char_(".") >> +qi::alnum ];
-
-
       divider %= lit("//") >> lexeme[+char_("-") >> qi::eol];
-
 
       timestamp %=
         repeat(4)[digit] >> char_("/")
@@ -328,9 +320,12 @@ BOOST_FUSION_ADAPT_STRUCT(
         ;
 
 
-      file_name %=
-        +(alnum | char_("_") | char_("."))
-        ;
+      file_name %= +(alnum | char_("_") | char_(".")) ;
+
+      file_path %= +(char_("/") >> file_name) ;
+
+      file_list %= file_path % lit("//");
+      file_line %= file_path >> lit("//");
 
       event %=
         lit("//") >> timestamp
@@ -343,29 +338,37 @@ BOOST_FUSION_ADAPT_STRUCT(
 
       tl_initial %=
         lit("//") >> "Mission Week:" >> int_
-        >> "//" >> "Timeline Name:" >> lexeme[+(qi::graph)]
+        >> "//" >> "Timeline Name:" >> file_name
         >> lit("Created:") >> timestamp
         >> "//" >> "Created with" >> one_liner
-        >> "//" >> "Initial pointing" >> RA >> DEC
+        >> "//" >> "Initial pointing"
+        >> RA
+        >> DEC
         >> "//" >> "Initial survey-related flight parameter settings"
         >> tl_rocking_profile
         >> -event
         >> "//"
-        >> "//" >> "Input files:"
-        >> "//" >> "TAKO database:" >> file_path % lit("//")
-        >> "//" >> "Spacecraft ephemeris:" >> file_path % lit("//")
-        >> "//" >> "SAA:" >> file_path % lit("//")
-        >> "//" >> "TDRSS ephemeris:" >> file_path % lit("//")
-        >> "//" >> "TDRSS contact schedule:" >> file_path % lit("//")
-        >> "//"
-        >> "//" >> "Previous ARR Threshold was" >> *qi::alpha
+        >> "// Input files:"
+        // I do not know how to parse these lists of file paths into vectors
+        // of strings. If necessary I will try again later. For now
+        // I'll just match everything up to empty line.
+        /* >> "// TAKO database:" >> */
+        /* *(-lit("//") >> file_line [phoenix::push_back(at_c<8>(_val), _1)]) */
+        /* >> "// Spacecraft ephemeris:" >> */
+        /* *(-lit("//") >> file_line [phoenix::push_back(at_c<9>(_val), _1)]) */
+        /* >> "// SAA:" >> */
+        /* *(-lit("//") >> file_line [phoenix::push_back(at_c<10>(_val), _1)]) */
+        /* >> "// TDRSS ephemeris:" >> file_list [at_c<11>(_val) = _1] */
+        /* >> "// TDRSS contact schedule:" >> file_list [at_c<12>(_val) = _1] */
+        /* >> "//" */
+        /* >> "//" >> "Previous ARR Threshold was" >> *qi::alpha [at_c<8>(_val) = _1] */
+
+        // This should match everything up to an empty line.
+        >> qi::eol >> qi::eol
         ;
 
-      command_param %= "("
-        >> lexeme[+(char_ - char_(")"))]
-        >> ")"
-        ;
 
+      command_param %= "(" >> lexeme[+(char_ - char_(")"))] >> ")" ;
 
       command %=
         timestamp
@@ -375,23 +378,23 @@ BOOST_FUSION_ADAPT_STRUCT(
         >>";"
         ;
 
+      header %= *(char_ - ";") >> ";";
 
-      header %=
-        file_name													// File Name
-        >> "," >> timestamp								// Creation Time
-        >> "," >> *alnum									// Mission Identifier
-        >> "," >> lexeme[*(alnum)]				// Originator
-        >> "," >> *(alnum | char_("."))		// Project Database Version
-        >> "," >> *alnum									// Destination processor
-        >> "," >> timestamp								// Start Time
-        >> "," >> timestamp								// Stop Time
-        >> "," >> *alnum									// Execute Flag
-        >> "," >> *alnum									// Timeline type
-        >> "," >> qi::repeat(2)[digit]		// Version number
-        >> "," >> file_name								// Reference Timeline Filename
-        >> "," >> lexeme[+(char_ - ";")]	// Comment
-        >> ";"
-        ;
+      /* header %= file_name                 // File Name */
+      /*   >> "," >> timestamp               // Creation Time */
+      /*   >> "," >> *alnum                  // Mission Identifier */
+      /*   >> "," >> *(alnum)                // Originator */
+      /*   >> "," >> *(alnum | char_("."))   // Project Database Version */
+      /*   >> "," >> *alnum                  // Destination processor */
+      /*   >> "," >> timestamp               // Start Time */
+      /*   >> "," >> timestamp               // Stop Time */
+      /*   >> "," >> *alnum                  // Execute Flag */
+      /*   >> "," >> *alnum                  // Timeline type */
+      /*   /1* >> "," >> *alnum                  // Version number *1/ */
+      /*   /1* >> "," >> file_name               // Reference Timeline Filename *1/ */
+      /*   >> "," >> lexeme[+(char_ - ";")]  // Comment */
+      /*   >> ";" */
+      /*   ; */
 
       generic_comment %= !(event | tl_initial)
         >> lit("//")
@@ -416,24 +419,27 @@ BOOST_FUSION_ADAPT_STRUCT(
 
     // Rules to parse compnents of a timeline file into their various
     // data structures needed for the timeline object.
-    qi::rule<Iterator, timeline_header(), ascii::space_type> header;
-    qi::rule<Iterator, initial(), ascii::space_type> tl_initial;
+    qi::rule<Iterator, std::string(), ascii::space_type> header;
+    qi::rule<Iterator, timeline_initial(), ascii::space_type> tl_initial;
     qi::rule<Iterator, timeline_event(), ascii::space_type> event;
     qi::rule<Iterator, opt_evt_fields(), ascii::space_type> tl_opt_evt_fields;
     qi::rule<Iterator, rocking_profile(), ascii::space_type> tl_rocking_profile;
     qi::rule<Iterator, rockprofile_pair(), ascii::space_type> rocktime_angle;
 
     // Sub-parsers
+    typedef qi::rule<Iterator, std::vector<std::string>(), ascii::space_type> str_vec;
     typedef qi::rule<Iterator, std::string(), ascii::space_type> string_rule;
     typedef qi::rule<Iterator, int, ascii::space_type> int_rule;
     typedef qi::rule<Iterator, double, ascii::space_type> double_rule;
 
+    str_vec file_list;
+
     string_rule one_liner;
     string_rule file_path;
-    string_rule command_param;
-
-    string_rule timestamp;
+    string_rule file_line;
     string_rule file_name;
+    string_rule command_param;
+    string_rule timestamp;
 
     double_rule RA;
     double_rule DEC;
@@ -460,8 +466,8 @@ public:
 
   // Member Variables
   bool success;
-  timeline_header header;
-  initial init;
+  std::string header;
+  timeline_initial init;
   std::vector<timeline_event> events;
 };
 
